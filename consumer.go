c12ac1d5be9eef364808721715e38596ee7e1054
sumory <sumory.wu@gmail.com>
2015-08-24 19:33:34 +0800

    init

---
+++ consumer.go
@@ -0,0 +1,47 @@
+package main
+
+
+import (
+	"github.com/jrallison/go-workers"
+	"fmt"
+	"github.com/sumory/go_relation/common"
+)
+
+func Get(message *workers.Msg) {
+	fmt.Printf("%+v\n", message)
+}
+
+
+func Post(message *workers.Msg) {
+	fmt.Printf("%+v\n", message)
+}
+
+type myMiddleware struct{}
+
+func (r *myMiddleware) Call(queue string, message *workers.Msg, next func() bool) (acknowledge bool) {
+	fmt.Printf("message %s start %s \n", message.Jid(), common.Now())
+	acknowledge = next()
+	fmt.Printf("message %s finish %s \n", message.Jid(), common.Now())
+	return
+}
+
+func main() {
+	workers.Configure(map[string]string{
+		"server":  "192.168.100.185:6389",
+		"database":  "0",
+		"pool":    "30",
+		// unique process id for this instance of workers (for proper recovery of inprogress jobs on crash)
+		"process": "1",
+	})
+
+	//workers.Middleware.Append(&myMiddleware{})
+
+	workers.Process("get_queue", Get, 10)
+	workers.Process("post_queue", Post, 10)
+
+
+	go workers.StatsServer(8080)
+
+	// Blocks until process is told to exit via unix signal
+	workers.Run()
+}
---
+++ producer.go
@@ -0,0 +1,48 @@
+package main
+
+
+import (
+	"github.com/jrallison/go-workers"
+	"time"
+)
+
+
+func main() {
+	workers.Configure(map[string]string{
+		"server":  "192.168.100.185:6389",
+		"database":  "0",
+		"pool":    "30",
+		// unique process id for this instance of workers (for proper recovery of inprogress jobs on crash)
+		"process": "1",
+	})
+
+
+
+		for {
+			time.Sleep(2*time.Second)
+			workers.Enqueue("get_queue", "Get", map[string]interface{}{
+				"harId":"abc",
+				"harCotent":map[string]interface{}{
+					"url":"http://baidu.com",
+					"headers":"abc",
+				},
+			})
+		}
+
+
+	go func(){
+		for {
+			time.Sleep(3*time.Second)
+			workers.Enqueue("post_queue", "Post", []int{1, 2})
+		}
+	}()
+
+
+	go workers.StatsServer(8081)
+
+	workers.Run()
+
+	select {
+
+	}
+}
